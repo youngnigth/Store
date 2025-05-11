7c3564bd3dea526a66cb772e0e8ae13a482939e8
youngnigth <youngdesert01@gmail.com>
2025-05-10 21:12:33 -0500

    Update products.js

--- products.js
+++ products.js
@@ -3,16 +3,16 @@ const products = [
     id: 1,
     name: "Street Tee",
     price: 25,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Minimalist tee in ultra-soft combed cotton.",
-    category: "Shirts",
+    category: "Hoodies",
     tags: ["tee", "cotton", "minimal"]
   },
   {
     id: 2,
     name: "Night Hoodie",
     price: 45,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Heavyweight hoodie for creators.",
     category: "Hoodies",
     tags: ["hoodie", "black", "warm"]
@@ -21,7 +21,7 @@ const products = [
     id: 3,
     name: "Creator Mug A",
     price: 15,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Matte ceramic mug for your ritual.",
     category: "Hoodies",
     tags: ["mug", "ceramic", "morning"]
@@ -30,7 +30,7 @@ const products = [
     id: 4,
     name: "Creator Mug B",
     price: 15,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Matte ceramic mug for your ritual.",
     category: "Mugs",
     tags: ["mug", "matte", "ritual"]
@@ -39,7 +39,7 @@ const products = [
     id: 5,
     name: "Creator Mug C",
     price: 15,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Matte ceramic mug for your ritual.",
     category: "Mugs",
     tags: ["mug", "ceramic", "daily"]
@@ -48,25 +48,25 @@ const products = [
     id: 6,
     name: "Creator Mug D",
     price: 15,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Matte ceramic mug for your ritual.",
-    category: "Shirts",
+    category: "Mugs",
     tags: ["mug", "coffee", "desk"]
   },
   {
     id: 7,
     name: "Creator Mug E",
     price: 15,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Matte ceramic mug for your ritual.",
-    category: "Mugs",
+    category: "Shirts",
     tags: ["mug", "latte", "ceramic"]
   },
   {
     id: 8,
     name: "Studio Shirt",
     price: 30,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Lightweight studio wear for creators.",
     category: "Shirts",
     tags: ["shirt", "white", "soft"]
@@ -75,25 +75,25 @@ const products = [
     id: 9,
     name: "Drop Beanie",
     price: 20,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Ribbed beanie for cool days.",
-    category: "Accessories",
+    category: "Shirts",
     tags: ["beanie", "warm", "headwear"]
   },
   {
     id: 10,
     name: "Creator Hoodie",
     price: 50,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Fleece-lined hoodie in rich black.",
-    category: "Hoodies",
+    category: "Accessories",
     tags: ["hoodie", "fleece", "black"]
   },
   {
     id: 9,
     name: "Drop Beanie",
     price: 20,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Ribbed beanie for cool days.",
     category: "Accessories",
     tags: ["beanie", "warm", "headwear"]
@@ -102,7 +102,7 @@ const products = [
     id: 9,
     name: "Drop Beanie",
     price: 20,
-    image: "images/photo1.jpg",
+    image: "photo1.jpg",
     description: "Ribbed beanie for cool days.",
     category: "Accessories",
     tags: ["beanie", "warm", "headwear"]
